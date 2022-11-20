e8af96a8ab964a0dda05208aebd86943064dfb37
John <johnfr14.dev@proton.me>
2022-11-20 21:04:43 +0900

    updating default ERCs contract

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -2,34 +2,79 @@
 pragma solidity ^0.8.9;
 
 // ERC20
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChestERC20 is ERC20 {
-    constructor() ERC20("ChestERC20", "cERC20") {}
+contract ChestERC20 is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor(string memory name, string memory ticker) ERC20(name, ticker) {}
 
-    function mint(uint256 amount) external {
-        _mint(msg.sender, amount);
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
 
 // ERC721
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ChestERC721 is ERC721, Ownable {
+contract ChestERC721 is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("ChestERC721", "cERC721") {}
+    constructor(string memory name, string memory ticker) ERC721(name, ticker) {}
 
-    function safeMint(address to) public onlyOwner {
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
-        _safeMint(to, tokenId);
         _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
 
@@ -37,12 +82,23 @@ contract ChestERC721 is ERC721, Ownable {
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract ChestERC1155 is ERC1155, Ownable {
-    constructor() ERC1155("") {}
+contract ChestERC1155 is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
+    constructor(string memory uri) ERC1155(uri) {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
@@ -57,5 +113,13 @@ contract ChestERC1155 is ERC1155, Ownable {
         onlyOwner
     {
         _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
